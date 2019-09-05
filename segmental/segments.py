d067d3fa9626812219ff796dc907564cd9c6a39a
patrick <patrickaaleask@gmail.com>
2019-09-05 18:04:57 +0100

    Added tests for object

--- segmental/segments.py
+++ segmental/segments.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 class Segments:
-    def __init__(self, arr):
+    def __init__(self, arr: np.ndarray):
         self._arr = arr
 
-    def __array__(self):
+    def __array__(self) -> np.ndarray:
         return self._arr
