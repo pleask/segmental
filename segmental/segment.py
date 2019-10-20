114a02dc88bf54e7d1e29f269a3cd470de67d621
patrick <patrickaaleask@gmail.com>
2019-10-20 12:18:28 +0100

    Implements skeleton SegmentCollection container

--- segmental/segment.py
+++ segmental/segment.py
@@ -8,3 +8,12 @@ class Segment:
 
     def __array__(self) -> np.ndarray:
         return self._arr
+
+
+class SegmentCollection:
+    def __init__(self, segments: List[np.ndarray]):
+        self._segments = segments
+
+    @property
+    def segments(self):
+        return self._segments
--- segmental/segment_test.py
+++ segmental/segment_test.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from .segments import Segments
+from .segment import Segment, SegmentCollection
 
 
 class TestSegment(unittest.TestCase):
@@ -8,6 +8,13 @@ class TestSegment(unittest.TestCase):
         array = np.zeros(1)
         segment = Segment(array)
         self.assertEqual(array, segment)
+
+
+class TestSegmentCollection(unittest.TestCase):
+    def test_init(self):
+        segments = [np.zeros(1), np.zeros(1)]
+        segment_collection = SegmentCollection(segments)
+        self.assertEqual(segments, segment_collection.segments)
 
 if __name__ == "__main__":
     unittest.main()
