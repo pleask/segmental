44bac364a2f04bf44a036201b5e020c30f25140a
patrick <patrickaaleask@gmail.com>
2019-10-20 13:26:07 +0100

    Adds support for u funcs and array operators

--- segmental/segment.py
+++ segmental/segment.py
@@ -1,14 +1,49 @@
 import numpy as np
+import numbers
 from typing import List
 
 
-class Segment:
+class Segment(np.lib.mixins.NDArrayOperatorsMixin):
     def __init__(self, arr: np.ndarray):
         self._arr = arr
+
+    @property
+    def value(self) -> np.ndarray:
+        return self._arr
 
     def __array__(self) -> np.ndarray:
         return self._arr
 
+    _HANDLED_TYPES = (np.ndarray, numbers.Number)
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+        out = kwargs.get('out', ())
+        for x in inputs + out:
+            # Only support operations with instances of _HANDLED_TYPES.
+            # Use Segment instead of type(self) for isinstance to
+            # allow subclasses that don't override __array_ufunc__ to
+            # handle Segment objects.
+            if not isinstance(x, self._HANDLED_TYPES + (Segment,)):
+                return NotImplemented
+
+        # Defer to the implementation of the ufunc on unwrapped values.
+        inputs = tuple(x.value if isinstance(x, Segment) else x
+                       for x in inputs)
+        if out:
+            kwargs['out'] = tuple(
+                x.value if isinstance(x, Segment) else x
+                for x in out)
+        result = getattr(ufunc, method)(*inputs, **kwargs)
+
+        if type(result) is tuple:
+            # multiple return values
+            return tuple(type(self)(x) for x in result)
+        elif method == 'at':
+            # no return value
+            return None
+        else:
+            # one return value
+            return type(self)(result)
 
 class SegmentCollection:
     def __init__(self, segments: List[np.ndarray]):
--- segmental/segment_test.py
+++ segmental/segment_test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
+from numpy.testing import assert_array_equal
 from .segment import Segment, SegmentCollection
-
 
 class TestSegment(unittest.TestCase):
     def test_as_array(self):
@@ -9,12 +9,22 @@ class TestSegment(unittest.TestCase):
         segment = Segment(array)
         self.assertEqual(array, segment)
 
+    def test_addition(self):
+        segment = Segment(np.ones(1)) + 2
+        array = np.ones(1) + 2
+        assert_array_equal(array, segment)
+
+    def test_multiplication(self):
+        segment = Segment(np.int_([1, 0]))
+        assert_array_equal(np.int_([5, 0]), segment * np.int_([5,1]))
+        
 
 class TestSegmentCollection(unittest.TestCase):
     def test_init(self):
         segments = [np.zeros(1), np.zeros(1)]
         segment_collection = SegmentCollection(segments)
-        self.assertEqual(segments, segment_collection.segments)
+        assert_array_equal(segments, segment_collection.segments)
+
 
 if __name__ == "__main__":
     unittest.main()
